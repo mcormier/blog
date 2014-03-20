9ddf68e4680bda3769c59d5080164bdb200aa5a7
Matthieu Cormier <mcormier@cs.dal.ca>
2014-03-20 19:55:15 -0300

    Set stuff up, sit back and write.

--- monitor.fish
+++ monitor.fish
@@ -4,7 +4,8 @@ set -U hostname (hostname)
 
 switch $hostname
   case '*veg*'
-    echo "TODO -- what port ..."
+    # Give Jekyll 10 seconds to load and then launch local site in browser
+    fish -c "sleep 10 ; open -a Safari http://127.0.0.1:4000"&
     jekyll serve --watch --drafts --destination /Users/mcormier/Sites/weblog/
   case '*preen*'
     jekyll build --watch --drafts
