b5cb130499b3b4c33f8c7ff88614eb10162d087b
Matthieu Cormier <mcormier@cs.dal.ca>
2014-04-05 20:13:07 -0300

    Add comment teaser message.

--- monitor.fish
+++ monitor.fish
@@ -4,6 +4,7 @@ set -U hostname (hostname)
 
 switch $hostname
   case '*veg*'
+  case '*cocoa*'
     # Give Jekyll 10 seconds to load and then launch local site in browser
     fish -c "sleep 10 ; open -a Safari http://127.0.0.1:4000"&
     jekyll serve --watch --drafts --destination /Users/mcormier/Sites/weblog/
