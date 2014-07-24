04717543ed51ae683e51d5392dbd18b710ce87b2
Matthieu Cormier <mcormier@cs.dal.ca>
2014-07-24 17:15:07 -0300

    add auto edit command

--- monitor.fish
+++ monitor.fish
@@ -5,6 +5,9 @@ set -U hostname (hostname)
 switch $hostname
   case '*veg*'
   case '*cocoa*'
+    osascript -e 'tell app "Terminal"
+                  do script "vi /Users/mcormier/Sites/jekyll/css/main.css"
+              end tell'
     # Give Jekyll 10 seconds to load and then launch local site in browser
     fish -c "sleep 10 ; open -a Safari http://127.0.0.1:4000"&
     jekyll serve --watch --drafts --destination /Users/mcormier/Sites/weblog/
