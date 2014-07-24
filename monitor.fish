0a42a6badac7691b89a4740716aeb5d94118e3bb
Matthieu Cormier <mcormier@cs.dal.ca>
2014-07-24 18:36:01 -0300

    Auto open common files in VI using applescript

--- monitor.fish
+++ monitor.fish
@@ -1,13 +1,20 @@
 #!/usr/bin/env fish
 
+function autoEditFile --argument-names fileToOpen
+    osascript -e 'on run args
+                    set script_command to "vi " & (item 1 of args)
+                    tell app "Terminal"
+                      do script script_command
+                    end tell
+                  end run'  $fileToOpen
+end
+
 set -U hostname (hostname)
 
 switch $hostname
   case '*veg*'
   case '*cocoa*'
-    osascript -e 'tell app "Terminal"
-                  do script "vi /Users/mcormier/Sites/jekyll/css/main.css"
-              end tell'
+    autoEditFile "/Users/mcormier/Sites/jekyll/css/main.css /Users/mcormier/Sites/jekyll/index.html"
     # Give Jekyll 10 seconds to load and then launch local site in browser
     fish -c "sleep 10 ; open -a Safari http://127.0.0.1:4000"&
     jekyll serve --watch --drafts --destination /Users/mcormier/Sites/weblog/
