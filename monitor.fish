a4d2b9ffe7b228c7c481ed6b67852ad986b4fac4
Matthieu Cormier <mcormier@cs.dal.ca>
2014-07-26 20:43:22 -0300

    make applescript spawn function more generic

--- monitor.fish
+++ monitor.fish
@@ -1,12 +1,12 @@
 #!/usr/bin/env fish
 
-function autoEditFile --argument-names fileToOpen
+function spawnTerminal --argument-names shellCommands
     osascript -e 'on run args
-                    set script_command to "vi " & (item 1 of args)
+                    set script_command to (item 1 of args)
                     tell app "Terminal"
                       do script script_command
                     end tell
-                  end run'  $fileToOpen
+                  end run'  $shellCommands
 end
 
 set -U hostname (hostname)
@@ -14,7 +14,7 @@ set -U hostname (hostname)
 switch $hostname
   case '*veg*'
   case '*cocoa*'
-    autoEditFile "/Users/mcormier/Sites/jekyll/css/main.css /Users/mcormier/Sites/jekyll/index.html"
+    spawnTerminal "cd Sites/jekyll; vi css/main.css index.html"
     # Give Jekyll 10 seconds to load and then launch local site in browser
     fish -c "sleep 10 ; open -a Safari http://127.0.0.1:4000"&
     jekyll serve --watch --drafts --destination /Users/mcormier/Sites/weblog/
