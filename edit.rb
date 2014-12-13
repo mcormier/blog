fb929e4a4edf1851a7e809c328603c300ad99b55
Matthieu Cormier <mcormier@cs.dal.ca>
2014-12-13 13:31:08 -0400

    upgrade to alpha ppcurses

--- edit.rb
+++ edit.rb
@@ -1,36 +1,9 @@
 #!/usr/bin/env ruby
 
-gem 'ppcurses', '=0.0.25'
+gem 'ppcurses', '=0.1.0'
 require 'ppcurses'
 
 require 'highline/import'
-
-class ScriptMenu < PPCurses::Menu
-  attr_reader :selection
-  attr_reader :pressed_enter
-
-  def menu_selection
-      while 1
-        c = @win.getch
-
-        if c == 27  # ESCAPE 
-          @pressed_enter = false
-          self.hide()
-          break
-        end
-
-        if c == 10 # ENTER
-          @pressed_enter = true
-          self.hide()
-          break
-        end
-
-        self.handle_menu_selection(c)
-
-      end
-  end
-
-end
 
 def processCommand( todo  )
   cmd =  todo
@@ -81,7 +54,7 @@ end
 def display_menu
   @task_list = ["Write", "Tweak" ] 
 
-  @task_menu = ScriptMenu.new( @task_list, nil )
+  @task_menu = PPCurses::ChoiceMenu.new( @task_list )
   @task_menu.show()
   @task_menu.menu_selection()
 end
